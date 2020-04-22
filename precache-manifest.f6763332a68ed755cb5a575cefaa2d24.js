546eda2b27a10679141b03af8f923af4f4daddbe
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-04-22 14:43:16 +0300

    Updates

---
+++ precache-manifest.f6763332a68ed755cb5a575cefaa2d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d5220583117b7c64605d9681bedf60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "413678a47054509792ec",
+    "url": "./static/css/main.40f50ef4.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413678a47054509792ec",
+    "url": "./static/js/main.dea63c0f.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
